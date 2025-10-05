f2ca1522dbcf5db296390c54686b733f888f6256
Ali <ali.almuhtaseb@student.manchester.ac.uk>
2025-10-05 13:54:19 +0100

    feat: configure API Gateway endpoint in environment

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,7 +1,6 @@
 ﻿export const environment = {
   production: false,
-  // TODO: Update this URL after deploying API Gateway
-  apiUrl: 'https://your-api-gateway-url.execute-api.eu-central-1.amazonaws.com/prod',
+  apiUrl: 'https://abhyzb0eoe.execute-api.eu-central-1.amazonaws.com/prod',
   cognito: {
     region: 'eu-central-1',
     userPoolId: 'eu-central-1_QEBbXGvw4',
